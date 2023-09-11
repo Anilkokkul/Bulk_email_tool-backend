ede59319c3dc0de2a9b4390f139e73397a12e90f
Anilkokkul <anilkokkul1997@gmail.com>
2023-09-11 21:14:49 +0530

    added CORS

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
+const cors = require("cors");
 const { db } = require("./db/db.connect");
 const userRoutes = require("./Routes/auth.routes");
 const templateRoutes = require("./Routes/templates.route");
 const bulkEmailRoute = require("./Routes/bulk_email_route");
 const cookieParser = require("cookie-parser");
 db();
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(userRoutes);
