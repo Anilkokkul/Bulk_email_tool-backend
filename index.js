0adda130c1a2b94a016403617366a2258a884b8a
Anilkokkul <anilkokkul1997@gmail.com>
2023-09-12 23:38:46 +0530

    updated

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ const templateRoutes = require("./Routes/templates.route");
 const bulkEmailRoute = require("./Routes/bulk_email_route");
 const cookieParser = require("cookie-parser");
 db();
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(cookieParser());
+app.use(express.json());
 app.use(userRoutes);
 app.use(templateRoutes);
 app.use(bulkEmailRoute);
@@ -18,6 +24,10 @@ app.use(bulkEmailRoute);
 port = process.env.PORT || 8001;
 app.get("/", (req, res) => {
   res.status(200).send("API is running");
+});
+app.get("/set-cookie", (req, res) => {
+  res.cookie("foo", "bar");
+  res.send("cookis is set");
 });
 
 app.listen(port, () => {
