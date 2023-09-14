af2e82b3b6f3b9fd197833a05e058539cd325a47
Anilkokkul <anilkokkul1997@gmail.com>
2023-09-14 14:44:55 +0530

    updated

--- Controllers/auth.controllers.js
+++ Controllers/auth.controllers.js
@@ -71,10 +71,8 @@ exports.login = async (req, res) => {
           process.env.SECRET_KEY
         );
         res.cookie("accessToken", token, {
-          httpOnly: false,
+          httpOnly: true,
           secure: true,
-          sameSite: "none",
-          domaine: ".netlify.app",
           expires: new Date(Date.now() + 86400000),
         });
         return res.status(200).send({
