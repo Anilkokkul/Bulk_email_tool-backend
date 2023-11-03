b764a414f4c85516bb95307b9a9025e3d003ce93
Anilkokkul <anilkokkul1997@gmail.com>
2023-11-03 23:26:54 +0530

    updated

--- models/user.model.js
+++ models/user.model.js
@@ -1,30 +1,32 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: [true, "Entered email is already registered"],
-    trim: true,
-  },
-  mobileNumber: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  hashedPassword: {
-    type: String,
-    required: true,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: [true, "Entered email is already registered"],
+      trim: true,
+    },
+    mobileNumber: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    hashedPassword: {
+      type: String,
+      required: true,
+      trim: true,
+    },
   },
   {
     timestamps: true,
   }
-});
+);
 
 module.exports = mongoose.model("Users", userSchema);
